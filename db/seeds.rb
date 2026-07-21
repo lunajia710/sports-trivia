2c7e0eef25592ede596854117e0a633c778be16f
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-21 21:32:36 +1000

    add username to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+tom = User.create!(username: "tom", email: "tom@example.com", password: "secret123")
