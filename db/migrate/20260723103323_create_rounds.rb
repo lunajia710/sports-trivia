e2b7d2298460592d1a9ba9ce1ca95d18e12c9fc2
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-23 21:02:04 +1000

    migrate round

--- db/migrate/20260723103323_create_rounds.rb
+++ db/migrate/20260723103323_create_rounds.rb
@@ -1,7 +1,7 @@
 class CreateRounds < ActiveRecord::Migration[8.1]
   def change
     create_table :rounds do |t|
-      t.integer :score
+      t.integer :score, null: false, default: 0
       t.references :deck, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
