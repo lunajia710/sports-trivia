6d71f62a0a94fd805de3d46b375b2a71c9bf4be8
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-24 22:22:03 +1000

    updated rounds resources

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -3,9 +3,10 @@ class RoundsController < ApplicationController
     @round = Round.find(params[:id])
     answers = @round.answers
     answered_question_ids = answers.map { |answer| answer.question_id }
-    deck = @round.deck
-    questions = deck.questions
+    @deck = @round.deck
+    questions = @deck.questions
     unanswered_questions = questions.where.not(id: answered_question_ids)
     @question = unanswered_questions.first
+    # redirect_to deck_path(@deck) and return if @question.nil?
   end
 end
