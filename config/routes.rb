3d41d7eb37b3d74b961445a9c4dccc2198b0db27
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-25 14:46:42 +1000

    home routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: "pages#home"
+  root to: "decks#index"
   # get "rounds/:id", to: "rounds#show", as: :round_path
   get "questions/index"
   get "my_deck", to: "pages#my_deck"
