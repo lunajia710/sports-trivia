b6e5518e83abcd30885ecb6d21618a7fcc3cf541
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-28 20:05:11 +1000

    scaffold message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+  SYSTEM_PROMPT = "You are a trivia quiz master."
+
+  def create
+    @chat = current_user.chats.find(params[:chat_id])
+    @deck = @chat.deck
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
+
+    if @message.save
+      ruby_llm_chat = RubyLLM.chat
+      response = ruby_llm_chat.ask(@message.content)
+      Message.create(role: "assistant", content: response.content, chat: @chat)
+    else
+    end
+  end
 end
