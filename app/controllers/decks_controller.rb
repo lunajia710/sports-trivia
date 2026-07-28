6871fc680434b7e7fcf5336970be0e0e37bef319
Luna Jia <xiaotong_jia@yahoo.com>
2026-07-28 11:53:37 +1000

    a new deck page and create a new deck, redirect to deck/:id

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,22 +3,35 @@ class DecksController < ApplicationController
     @decks = Deck.all
   end
 
-  def show
-    @deck = Deck.find(params[:id])
-    @leaderboard = @deck.rounds.includes(:user).order(score: :desc).limit(10)
-  end
-
   def new
     @deck = Deck.new
   end
 
   def create
     # user types name of deck and creates a new deck with that name
-    @deck = Deck.new
+    @deck = Deck.new(deck_params)
+    @deck.user = current_user
+
+    if @deck.save
+      redirect_to deck_path(@deck)
+    else
+      render :new, status: 422
+    end
     # in response to user givcing deck name
     # create a chat with four questions and four answers per question, one correct, three incorrect
-    @chat = RubyLLM.chat
+    # @chat = RubyLLM.chat
     # create a message with the prompt to create four questions and four answers per question, one
+  end
+
+  def show
+    @deck = Deck.find(params[:id])
+    @leaderboard = @deck.rounds.includes(:user).order(score: :desc).limit(10)
+  end
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:title)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :questions, only: [:index]
 
-  resources :decks, only: [:index, :show] do
+  resources :decks, only: [:index, :new, :create, :show] do
     resources :rounds, only: [:create]
     resources :chats, only: [:create]
   end
