e3d1f36cd6998422eba3518cda63fea100e30460
Toshi Tsuchiyama <ttymtsnr0528@gmail.com>
2026-07-29 17:05:46 +0800

    after changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: :my_decks
   def home
   end
 
   def my_decks
+    @decks = current_user.decks.includes(:questions)
   end
 end
