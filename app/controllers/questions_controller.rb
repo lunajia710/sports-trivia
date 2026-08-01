52d67f04f3928da0294efb8e2c24b0600624c829
Abrahim Nasreddine <abrahim.n036@gmail.com>
2026-08-01 14:56:32 +1000

    delete question button and action completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,14 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def destroy
+    @deck = current_user.decks.find(params[:deck_id])
+    @question = @deck.questions.find(params[:id])
+    @chat = @deck.chat
+
+    @question.destroy!
+
+    redirect_to chat_path(@chat), notice: "Question deleted"
+  end
 end
